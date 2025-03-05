d2b39c2c69c41c65c50e279c49378568597d544d
Aboobacker F A <aboobackerfa580@gmail.com>
2025-03-05 13:09:39 +0530

    part3

--- index.js
+++ index.js
@@ -1,78 +1,129 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+const Contact = require('./models/contacts')
 
-app.use(express.json());
+const PORT = process.env.PORT || 3001
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
-});
-
-app.get("/api/info", (request, response) => {
-  const date = new Date();
-  response.send(`<p>Phonebook has ${persons.length} people<br>
-    ${date}</p>`);
-});
-
-app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    const errorMessages = Object.values(error.errors).map((err) => err.message)
+    return response.status(400).json({ error: errorMessages.join(' ') }) // Custom error response
   }
-});
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((person) => person.id !== id);
-  response.status(204).end();
-});
+  next(error)
+}
 
-app.post("/api/persons", (request, response) => {
-  const body = request.body;
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "content is missing",
-    });
-  } else if (persons.find((person) => person.name === body.name))
-    return response.status(400).json({
-      error: "name must be unique",
-    });
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-  const person = {
-    id: persons.length + 1,
+app.use(express.static('dist'))
+app.use(cors())
+app.use(express.json())
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
+
+app.get('/api/persons', (request, response) => {
+  Contact.find({}).then((result) => {
+    response.json(result)
+  })
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Contact.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Contact.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
+})
+
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ error: 'name missing' })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({ error: 'number missing' })
+  }
+
+  const contact = new Contact({
     name: body.name,
     number: body.number,
-  };
-  persons = persons.concat(person);
-  response.json(person);
-});
+  })
 
-const PORT = 3001;
+  contact
+    .save()
+    .then((savedContact) => {
+      response.json(savedContact)
+    })
+    .catch((error) => next(error))
+})
+
+// New PUT endpoint to handle updates
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing',
+    })
+  }
+
+  const contact = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Contact.findByIdAndUpdate(request.params.id, contact, {
+    new: true,
+    runValidators: true,
+    context: 'query',
+  })
+    .then((updatedContact) => {
+      if (updatedContact) {
+        response.json(updatedContact)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
+})
+
+app.get('/info', (request, response) => {
+  Contact.countDocuments({}).then((count) => {
+    const currentDate = new Date()
+    response.send(`
+        <p>Phonebook has info for ${count} people</p>
+        <p>${currentDate}</p>
+      `)
+  })
+})
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
